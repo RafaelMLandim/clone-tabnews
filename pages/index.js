f3a0d28feb042b9c0f1857595e6ef49702b96b85
Rafael Magalhaes Landim <134344408+RafaelMLandim@users.noreply.github.com>
2026-01-15 20:40:56 +0000

    Alteração do texto

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,52 @@
 
 function Home(){
-    return <h1>Bruno, por favor, necessito trabajo 🇲🇽🌮🎺🎉🌵</h1>
+    return <h1>Um homem estava dirigindo pela estrada e seu carro quebra perto de um mosteiro .
+Ele vai até o mosteiro, bate na porta e diz: Meu carro quebrou.
+Você acha que eu poderia passar a noite aqui?
+os monges graciosamente aceitam-no, alimentam-no e consertam o seu carro
+À medida que o homem tenta cair no sono , ele ouve um som estranho.
+Na manhã seguinte, ele pergunta aos monges que  som era aquele, mas eles dizem,
+Nós não podemos dizer-lhe. Você não é um monge.
+O homem ficou desapontado mas os agradece de qualquer maneira e fala sobre sua alegria.
+Alguns anos mais tarde,
+O mesmo homem tem seu carro quebrado em frente do mesmo mosteiro .
+Os monges novamente aceitam-no, alimentam -no , e consertam seu carro
+Naquela noite, ele ouve o mesmo barulho estranho que ouvira anos antes.
+na  manhã seguinte, ele pergunta o que é, mas os monges respondem,, Nós não podemos dizer .Você não é um monge.
+O homem diz: Tudo bem, tudo bem. Estou morrendo de vontade de saber.
+Se a única maneira que eu possa descobrir é me tornar um monge, me diz o que tenho que fazer?
+Os monges respondem, você deve viajar a terra e nos dizer quantas folhas de grama há  e o número exato de seixos de areia Quando você encontrar estes números,
+você vai se tornar um monge.
+O homem põe a sua tarefa.
+Alguns anos depois, ele retorna e bate na porta do monasterio.
+Ele diz,
+Tenho viajado a terra e  encontrado o que vocês me pediram
+145,236,284,232 lâminas de gramas e
+231,281,219,999,129,382 seixos de areia na terra.
+Os monges respondem, parabéns. Você agora é um monge.
+Vamos agora mostrar-lhe o caminho para o som
+Os monges levam o homem a uma porta de madeira onde o monge principal diz,
+O som está bem atrás daquela porta.
+O homem estende a mão para a maçaneta, mas a porta está trancada.
+Ele diz, realmente engraçado. Posso ter a chave ?
+Os monges lhe dam a chave, e ele abre a porta.
+Atrás da porta de madeira esta uma outra porta feita de pedra.
+O homem exige a chave para a porta de pedra.
+Os monges lhe dam a chave e ele abre, apenas para encontrar uma porta feita  de rubi.
+Ele exige outra chave dos monges, que fornecem.
+Atrás dessa porta esta outra porta, esta feita de safira,
+Assim foi até que o homem tinha ido através de portas de esmeralda,
+prata,
+topázio,
+e ametista.
+Finalmente, os monges dizem, esta é a última chave para a última porta.
+O homem está aliviado...
+Ele desbloqueia o cadeado,
+gira a maçaneta,
+e atrás daquela porta, ele fica espantado por encontrar a fonte daquele estranho som
+mas infelizmente ele não pode dizer-lhe o que é porque você não é
+um monge.
+😂😂</h1>
 }
 
 export default Home;
