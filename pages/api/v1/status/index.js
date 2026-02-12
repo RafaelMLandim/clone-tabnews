1591557e0d7a24b0fcf06594439135b29df995b4
Rafael Magalhaes Landim <134344408+RafaelMLandim@users.noreply.github.com>
2026-02-12 16:50:21 +0000

    finish endpoit

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sun;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "bateu no endpoint parabens" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const maxConectionsdatabase = await database.query("SHOW max_connections");
+  const maxConectionsdatabaseValue =
+    maxConectionsdatabase.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const usedConectionsValue = usedConections.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_conections: parseInt(maxConectionsdatabaseValue),
+        used_conections: usedConectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
